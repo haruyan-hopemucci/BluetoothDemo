a007a25e4b6c7678a152b59224f486308b6f3962
haruyanhpm <haruyanhpm@gmail.com>
2020-07-09 11:40:34 +0900

    first commit

---
+++ BluetoothDemo/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace BluetoothDemo
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxNonPair = new System.Windows.Forms.ListBox();
+            this.bindingSource_nonPair = new System.Windows.Forms.BindingSource(this.components);
+            this.listBoxPaired = new System.Windows.Forms.ListBox();
+            this.bindingSource_Paired = new System.Windows.Forms.BindingSource(this.components);
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.button_pairing = new System.Windows.Forms.Button();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.button_remove = new System.Windows.Forms.Button();
+            this.Toggle_RefrashAsync = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource_nonPair)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource_Paired)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxNonPair
+            // 
+            this.listBoxNonPair.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxNonPair.DataSource = this.bindingSource_nonPair;
+            this.listBoxNonPair.FormattingEnabled = true;
+            this.listBoxNonPair.ItemHeight = 20;
+            this.listBoxNonPair.Location = new System.Drawing.Point(22, 42);
+            this.listBoxNonPair.Margin = new System.Windows.Forms.Padding(5);
+            this.listBoxNonPair.Name = "listBoxNonPair";
+            this.listBoxNonPair.Size = new System.Drawing.Size(197, 284);
+            this.listBoxNonPair.TabIndex = 0;
+            this.listBoxNonPair.Click += new System.EventHandler(this.listBox_Click);
+            // 
+            // listBoxPaired
+            // 
+            this.listBoxPaired.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxPaired.DataSource = this.bindingSource_Paired;
+            this.listBoxPaired.FormattingEnabled = true;
+            this.listBoxPaired.ItemHeight = 20;
+            this.listBoxPaired.Location = new System.Drawing.Point(229, 42);
+            this.listBoxPaired.Margin = new System.Windows.Forms.Padding(5);
+            this.listBoxPaired.Name = "listBoxPaired";
+            this.listBoxPaired.Size = new System.Drawing.Size(197, 324);
+            this.listBoxPaired.TabIndex = 1;
+            this.listBoxPaired.Click += new System.EventHandler(this.listBox_Click);
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_refresh.Location = new System.Drawing.Point(22, 379);
+            this.button_refresh.Margin = new System.Windows.Forms.Padding(5);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(125, 38);
+            this.button_refresh.TabIndex = 2;
+            this.button_refresh.Text = "Refresh";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // button_pairing
+            // 
+            this.button_pairing.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_pairing.Location = new System.Drawing.Point(157, 379);
+            this.button_pairing.Margin = new System.Windows.Forms.Padding(5);
+            this.button_pairing.Name = "button_pairing";
+            this.button_pairing.Size = new System.Drawing.Size(125, 38);
+            this.button_pairing.TabIndex = 3;
+            this.button_pairing.Text = "Pairing";
+            this.button_pairing.UseVisualStyleBackColor = true;
+            this.button_pairing.Click += new System.EventHandler(this.button_pairing_Click);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.propertyGrid1.Location = new System.Drawing.Point(443, 42);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(288, 377);
+            this.propertyGrid1.TabIndex = 4;
+            // 
+            // button_remove
+            // 
+            this.button_remove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_remove.Location = new System.Drawing.Point(292, 379);
+            this.button_remove.Margin = new System.Windows.Forms.Padding(5);
+            this.button_remove.Name = "button_remove";
+            this.button_remove.Size = new System.Drawing.Size(125, 38);
+            this.button_remove.TabIndex = 3;
+            this.button_remove.Text = "Remove";
+            this.button_remove.UseVisualStyleBackColor = true;
+            this.button_remove.Click += new System.EventHandler(this.button_remove_Click);
+            // 
+            // Toggle_RefrashAsync
+            // 
+            this.Toggle_RefrashAsync.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Toggle_RefrashAsync.Location = new System.Drawing.Point(22, 335);
+            this.Toggle_RefrashAsync.Name = "Toggle_RefrashAsync";
+            this.Toggle_RefrashAsync.Size = new System.Drawing.Size(125, 36);
+            this.Toggle_RefrashAsync.TabIndex = 5;
+            this.Toggle_RefrashAsync.Text = "RefrashAsync";
+            this.Toggle_RefrashAsync.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Toggle_RefrashAsync.UseVisualStyleBackColor = true;
+            this.Toggle_RefrashAsync.CheckedChanged += new System.EventHandler(this.Toggle_RefrashAsync_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NonPaired";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Paired";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(439, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Properties";
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 431);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Toggle_RefrashAsync);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.button_remove);
+            this.Controls.Add(this.button_pairing);
+            this.Controls.Add(this.button_refresh);
+            this.Controls.Add(this.listBoxPaired);
+            this.Controls.Add(this.listBoxNonPair);
+            this.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form_main";
+            this.Text = "Form_main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource_nonPair)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource_Paired)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxNonPair;
+        private System.Windows.Forms.ListBox listBoxPaired;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.Button button_pairing;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.Button button_remove;
+        private System.Windows.Forms.BindingSource bindingSource_nonPair;
+        private System.Windows.Forms.BindingSource bindingSource_Paired;
+        private System.Windows.Forms.CheckBox Toggle_RefrashAsync;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BluetoothDemo/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+/// <summary>
+/// Bluetooth デバイス探索、ペアリングを行うアプリのデモ。
+/// </summary>
+namespace BluetoothDemo
+{
+    public partial class Form_main : Form
+    {
+        BluetoothClient bc;
+        BluetoothClient bcAsync;
+        public Form_main()
+        {
+            InitializeComponent();
+            // BluetoothClient オブジェクトの生成
+            bc = new BluetoothClient();
+            // 未ペアリングデバイスを探索する際の所要時間を設定。デフォルトは10秒。
+            bc.InquiryLength = new TimeSpan(0, 0, 5); // この場合は5秒
+            // 非同期BluetoothClient オブジェクトの生成
+            bcAsync = new BluetoothClient();
+            // 未ペアリングデバイスを探索する際の所要時間を設定。デフォルトは10秒。
+            bcAsync.InquiryLength = new TimeSpan(0, 0, 2); // この場合は5秒
+            // ペアリングリクエストが発生した場合、引数のメソッドをコールバックする。
+            BluetoothWin32Authentication authenticator = new BluetoothWin32Authentication(Win32AuthCallbackHandler);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// ペアリングとサービスのインストールを実施。
+        /// </summary>
+        /// <param name="deviceInfo">対象のデバイス</param>
+        /// <returns></returns>
+        private bool Pairing(BluetoothDeviceInfo deviceInfo)
+        {
+            // ペアリングリクエスト。完了するまで待ち合わせる。
+            bool paired = BluetoothSecurity.PairRequest(deviceInfo.DeviceAddress, null);
+
+            // ペアリング失敗したらそこで終了
+            if (!paired)
+            {
+                return false;
+            }
+
+            deviceInfo = new BluetoothDeviceInfo(deviceInfo.DeviceAddress);
+
+            // ペアリングしたデバイスが対応しているサービス一覧を取得し、リストに記憶する。
+            List<Guid> serviceGuidList = new List<Guid>();
+
+            // L2CapProtocolにてGetServiceRecordsを実行するとデバイスが利用可能なサービス一覧を取得できる。
+            ServiceRecord[] serviceinfo = deviceInfo.GetServiceRecords(BluetoothService.L2CapProtocol);
+            foreach (var record in serviceinfo)
+            {
+                // サービスレコードをテキストでダンプ
+                //ServiceRecordUtilities.Dump(Console.Error, record);
+                // 取得したサービスレコードのうち、ServiceDescription のレコードを取得する。
+                ServiceAttribute sdpRecord = record.GetAttributeById(InTheHand.Net.Bluetooth.AttributeIds.UniversalAttributeId.ServiceDescription);
+                if (sdpRecord != null)
+                {
+                    // サービスのGuidを取得
+                    serviceGuidList.Add(sdpRecord.Value.GetValueAsElementArray()[0].GetValueAsUuid());
+                }
+            }
+            // サービスのインストール
+            foreach (Guid service in serviceGuidList)
+            {
+                try
+                {
+                    // 個々のServiceに対し、有効を設定する。
+                    deviceInfo.SetServiceState(service, true, true);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                    Console.Error.WriteLine(ex.StackTrace);
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// ペアリングボタンクリック
+        /// listBoxNonPairで選択されているデバイスとペアリングを実施。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_pairing_Click(object sender, EventArgs e)
+        {
+            BluetoothDeviceInfo deviceInfo = (BluetoothDeviceInfo)listBoxNonPair.SelectedItem;
+            if(deviceInfo != null)
+            {
+                Pairing(deviceInfo);
+            }
+        }
+
+        private void Btlist_refresh()
+        {
+            // Bluetoothデバイスの探索を行う。
+            // 引数：
+            // int maxDevices    : 探索するデバイスの最大数。指定の数で探索を打ち切る。
+            // bool authenticated: trueだと認証済み（ペアリング済み）のデバイス、falseだと認証していないデバイスを結果に含める。
+            // bool remembered   : trueだとホストに記録済みのデバイスのみを結果に含める（認証したことのあるデバイス以外無視すると同じ？）
+            // bool unknown      : falseだと詳細のわからないデバイスを結果に含めない。
+
+            // 未ペアリングのデバイスを探索し、結果を返す
+            BluetoothDeviceInfo[] devices_nonpaired = bc.DiscoverDevices(32, false, false, true);
+            // ペアリング済みのデバイスを探索し、結果を返す
+            BluetoothDeviceInfo[] devices_paired = bc.DiscoverDevices(32, true, false, false);
+
+            bindingSource_nonPair.DataSource = devices_nonpaired;
+            bindingSource_Paired.DataSource = devices_paired;
+            // ListBoxの設定
+            listBoxNonPair.DisplayMember = "DeviceName";
+            listBoxPaired.DisplayMember = "DeviceName";
+        }
+
+        /// <summary>
+        /// 非同期で未ペアリングデバイスを探索する
+        /// </summary>
+        private void Btlist_refrashAsync()
+        {
+            // 探索を開始。探索時間経過後コールバックメソッドが実行される。
+            bcAsync.BeginDiscoverDevices(32, false, true, true, true, RefrashAsyncCallbackHandler,"non-paired");
+            bcAsync.BeginDiscoverDevices(32, true, true, false, false, RefrashAsyncCallbackHandler, "paired");
+        }
+
+        /// <summary>
+        /// 非同期デバイス探索後コールバック
+        /// </summary>
+        /// <param name="ar"></param>
+        private void RefrashAsyncCallbackHandler(IAsyncResult ar)
+        {
+            if (ar.IsCompleted)
+            {
+                BluetoothDeviceInfo[] devices = bcAsync.EndDiscoverDevices(ar);
+                if((string)ar.AsyncState == "non-paired") { 
+                    Invoke((MethodInvoker)(()=>{
+                        bindingSource_nonPair.DataSource = devices;
+                        listBoxNonPair.DisplayMember = "DeviceName";
+                    }));
+                    // non-pair側のみ継続条件をつける。こちらのほうが遅いので。
+                    if (Toggle_RefrashAsync.Checked)
+                    {
+                        bcAsync.BeginDiscoverDevices(32, false, false, true, true, RefrashAsyncCallbackHandler, "non-paired");
+                    }
+                }
+                else if ((string)ar.AsyncState == "paired")
+                {
+                    Invoke((MethodInvoker)(() => {
+                        bindingSource_Paired.DataSource = devices;
+                        listBoxPaired.DisplayMember = "DeviceName";
+                    }));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 対象のデバイス情報をプロパティグリッドに表示させる
+        /// </summary>
+        /// <param name="deviceInfo"></param>
+        private void SetDeviceInfoToPropertyGrid(BluetoothDeviceInfo deviceInfo)
+        {
+            propertyGrid1.SelectedObject = deviceInfo;
+        }
+
+        /// <summary>
+        /// ペアリングリクエストが発生した際に呼び出される。
+        /// </summary>
+        private void Win32AuthCallbackHandler(Object sender, BluetoothWin32AuthenticationEventArgs e)
+        {
+            // pinの設定。デバイスによっては必須。
+            e.Pin = "0000";
+            // ペアリングの了承・拒否を設定できる。trueにしてメソッドを返すとペアリング了承にできる。
+            e.Confirm = true;
+        }
+        /// <summary>
+        /// refrashボタンクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+            Btlist_refresh();
+        }
+
+        /// <summary>
+        /// クリックしたアイテムをプロパティグリッドに表示する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBox_Click(object sender, EventArgs e)
+        {
+            SetDeviceInfoToPropertyGrid((BluetoothDeviceInfo)((ListBox)sender).SelectedItem);
+        }
+
+        /// <summary>
+        /// RefreshAsync
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Toggle_RefrashAsync_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox obj = (CheckBox)sender;
+            if (obj.Checked)
+            {
+                Btlist_refrashAsync();
+            }
+        }
+
+        /// <summary>
+        /// 引数デバイスのペアリング解除
+        /// </summary>
+        /// <param name="deviceInfo"></param>
+        /// <returns></returns>
+        private bool RemovePairing(BluetoothDeviceInfo deviceInfo)
+        {
+            return BluetoothSecurity.RemoveDevice(deviceInfo.DeviceAddress);
+        }
+
+        /// <summary>
+        /// removeボタンクリック
+        /// listBoxPairedで選択されているデバイスとペアリングを解除する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_remove_Click(object sender, EventArgs e)
+        {
+            BluetoothDeviceInfo deviceInfo = (BluetoothDeviceInfo)listBoxPaired.SelectedItem;
+            if(deviceInfo != null)
+            {
+                RemovePairing(deviceInfo);
+            }
+        }
+    }
+}
